572de62ebf3eab736815835e581fb2682e701e34
Sébastien TEXIER <sebastien.tex6@hotmail.fr>
2020-12-02 14:32:07 +0100

    Update temperature_humidity.ino
    test

--- temperature_humidity/temperature_humidity.ino
+++ temperature_humidity/temperature_humidity.ino
@@ -46,10 +46,18 @@ void loop() {
   delay(1000);
 }
 
+/**
+ * Lit la température et le taux d'humidité mesuré par un capteur DHT11.
+ *
+ * @param pin Broche sur laquelle est câblée le capteur.
+ * @param temperature Pointeur vers la variable stockant la température.
+ * @param humidity Pointeur vers la variable stockant le taux d'humidité.
+ * @return DHT_SUCCESS si aucune erreur, DHT_TIMEOUT_ERROR en cas de timeout, ou DHT_CHECKSUM_ERROR en cas d'erreur de checksum.
+ */
 byte readDHT11(byte pin, float* temperature, float* humidity) {
   
   /* Lit le capteur */
-  byte data[BROCHE_CAPTEUR];
+  byte data[5];
   byte ret = readDHTxx(pin, data, 18, 1000);
   
   /* Détecte et retourne les erreurs de communication */
@@ -71,3 +79,96 @@ byte readDHTxx(byte pin, byte* data, unsigned long start_time, unsigned long tim
   data[0] = data[1] = data[2] = data[3] = data[4] = 0;
   // start_time est en millisecondes
   // timeout est en microsecondes
+ 
+  /* Conversion du numéro de broche Arduino en ports / masque binaire "bas niveau" */
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+  volatile uint8_t *ddr = portModeRegister(port);   // Registre MODE (INPUT / OUTPUT)
+  volatile uint8_t *out = portOutputRegister(port); // Registre OUT (écriture)
+  volatile uint8_t *in = portInputRegister(port);   // Registre IN (lecture)
+  
+  /* Conversion du temps de timeout en nombre de cycles processeur */
+  unsigned long max_cycles = microsecondsToClockCycles(timeout);
+ 
+  /* Evite les problèmes de pull-up */
+  *out |= bit;  // PULLUP
+  *ddr &= ~bit; // INPUT
+  delay(100);   // Laisse le temps à la résistance de pullup de mettre la ligne de données à HIGH
+ 
+  /* Réveil du capteur */
+  *ddr |= bit;  // OUTPUT
+  *out &= ~bit; // LOW
+  delay(start_time); // Temps d'attente à LOW causant le réveil du capteur
+  // N.B. Il est impossible d'utilise delayMicroseconds() ici car un délai
+  // de plus de 16 millisecondes ne donne pas un timing assez précis.
+  
+  /* Portion de code critique - pas d'interruptions possibles */
+  noInterrupts();
+  
+  /* Passage en écoute */
+  *out |= bit;  // PULLUP
+  delayMicroseconds(40);
+  *ddr &= ~bit; // INPUT
+ 
+  /* Attente de la réponse du capteur */
+  timeout = 0;
+  while(!(*in & bit)) { /* Attente d'un état LOW */
+    if (++timeout == max_cycles) {
+        interrupts();
+        return DHT_TIMEOUT_ERROR;
+      }
+  }
+    
+  timeout = 0;
+  while(*in & bit) { /* Attente d'un état HIGH */
+    if (++timeout == max_cycles) {
+        interrupts();
+        return DHT_TIMEOUT_ERROR;
+      }
+  }
+
+  /* Lecture des données du capteur (40 bits) */
+  for (byte i = 0; i < 40; ++i) {
+ 
+    /* Attente d'un état LOW */
+    unsigned long cycles_low = 0;
+    while(!(*in & bit)) {
+      if (++cycles_low == max_cycles) {
+        interrupts();
+        return DHT_TIMEOUT_ERROR;
+      }
+    }
+
+    /* Attente d'un état HIGH */
+    unsigned long cycles_high = 0;
+    while(*in & bit) {
+      if (++cycles_high == max_cycles) {
+        interrupts();
+        return DHT_TIMEOUT_ERROR;
+      }
+    }
+    
+    /* Si le temps haut est supérieur au temps bas c'est un "1", sinon c'est un "0" */
+    data[i / 8] <<= 1;
+    if (cycles_high > cycles_low) {
+      data[i / 8] |= 1;
+    }
+  }
+  
+  /* Fin de la portion de code critique */
+  interrupts();
+ 
+  /*
+   * Format des données :
+   * [1, 0] = humidité en %
+   * [3, 2] = température en degrés Celsius
+   * [4] = checksum (humidité + température)
+   */
+   
+  /* Vérifie la checksum */
+  byte checksum = (data[0] + data[1] + data[2] + data[3]) & 0xff;
+  if (data[4] != checksum)
+    return DHT_CHECKSUM_ERROR; /* Erreur de checksum */
+  else
+    return DHT_SUCCESS; /* Pas d'erreur */
+}
