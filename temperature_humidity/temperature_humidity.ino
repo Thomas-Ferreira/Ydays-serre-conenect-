78a2745477287862e9c62c68c6418dd3313a819f
Sébastien TEXIER <sebastien.tex6@hotmail.fr>
2020-12-02 14:27:45 +0100

    Update temperature_humidity.ino
    modif

--- temperature_humidity/temperature_humidity.ino
+++ temperature_humidity/temperature_humidity.ino
@@ -12,7 +12,7 @@ void setup() {
  
   /* Initialisation du port série */
   Serial.begin(115200);
-  Serial.println(F("Demo DHT11 et DHT22"));
+  Serial.println(F("Test DH11"));
   
   /* Place la broche du capteur en entrée avec pull-up */
   pinMode(BROCHE_CAPTEUR, INPUT_PULLUP);
@@ -45,3 +45,29 @@ void loop() {
   /* Une mesure par seconde */
   delay(1000);
 }
+
+byte readDHT11(byte pin, float* temperature, float* humidity) {
+  
+  /* Lit le capteur */
+  byte data[BROCHE_CAPTEUR];
+  byte ret = readDHTxx(pin, data, 18, 1000);
+  
+  /* Détecte et retourne les erreurs de communication */
+  if (ret != DHT_SUCCESS) 
+    return ret;
+    
+  /* Calcul la vraie valeur de la température et de l'humidité */
+  *humidity = data[0];
+  *temperature = data[2];
+
+  /* Ok */
+  return DHT_SUCCESS;
+}
+
+/**
+ * Fonction bas niveau permettant de lire la température et le taux d'humidité (en valeurs brutes) mesuré par un capteur DHTxx.
+ */
+byte readDHTxx(byte pin, byte* data, unsigned long start_time, unsigned long timeout) {
+  data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+  // start_time est en millisecondes
+  // timeout est en microsecondes
