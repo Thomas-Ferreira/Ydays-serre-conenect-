fa0c3ae7d3b06efc94fcc330eb6e40a55f37027f
Sébastien TEXIER <sebastien.tex6@hotmail.fr>
2020-12-16 14:31:55 +0100

    maj water sensor

--- test_capteur-eau/test_capteur-eau.ino
+++ test_capteur-eau/test_capteur-eau.ino
@@ -1,24 +1,32 @@
-#define cactus D5
+#define LEDAlert D5
+#define LEDFull D6
 #define waterSensor D0
-// Le test ce fait avec celui du sol car le water sensor ne marche pas
+// Le test se fait avec le capteur du sol car le water sensor ne marche pas
 
 void setup() {
   Serial.begin(9600);
   
-  pinMode(cactus, OUTPUT);
+  pinMode(LEDAlert, OUTPUT);
+  pinMode(LEDFull, OUTPUT);
   pinMode(waterSensor, INPUT);
   Serial.println("Demarrage water sensor");
 }
 
 void loop() {
-  Serial.println(digitalRead(waterSensor));
-
+  // Détecte de l'eau
   if( digitalRead(waterSensor) == LOW) {
-    // Détecte de l'eau
-    digitalWrite(cactus,LOW);
+    
+    digitalWrite(LEDAlert,LOW);
+    Serial.println("Le réservoir contien de l'eau");
+    digitalWrite(LEDFull,HIGH);
+    delay(1000);
   }else {
     // Ne détecte pas d'eau
-    digitalWrite(cactus,HIGH);
+    digitalWrite(LEDFull,LOW);
+    Serial.println("Le réservoir n'a plus d'eau");
+    digitalWrite(LEDAlert,HIGH);
+    delay(1000);
+    digitalWrite(LEDAlert,LOW);
+    delay(1000);
   }
-  delay(1000);
 }
